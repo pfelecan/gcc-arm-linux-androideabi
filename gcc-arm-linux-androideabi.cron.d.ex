27242eda0a6b6afaf7f1186129b0d35a14cbe979
Dmitrijs Ledkovs <dmitrijs.ledkovs@canonical.com>
2013-07-04 23:37:17 +0100

    Uss gcc-4.7-source.

--- gcc-arm-linux-androideabi.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the gcc-arm-linux-androideabi package
-#
-0 4	* * *	root	[ -x /usr/bin/gcc-arm-linux-androideabi_maintenance ] && /usr/bin/gcc-arm-linux-androideabi_maintenance
--- menu.ex
+++
@@ -1,2 +0,0 @@
-?package(gcc-arm-linux-androideabi):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="gcc-arm-linux-androideabi" command="/usr/bin/gcc-arm-linux-androideabi"
